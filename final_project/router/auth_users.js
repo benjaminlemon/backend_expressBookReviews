2b3487f3c9dfc9982460215d5bce5ada4be80ad6
benjaminlemon <benjamin.lemon1@gmail.com>
2024-04-22 15:09:56 -0600

    complete Task 8/9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -64,8 +64,48 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const review = req.body.review;
+  const username = req.session.authorization.username;
+
+  if (books[isbn]) {
+    books[isbn]["reviews"][username] = review;
+    console.log(books[isbn]);
+    return res
+      .status(200)
+      .send(
+        JSON.stringify(books[isbn]["title"]) +
+          "\n" +
+          JSON.stringify(books[isbn]["reviews"], null, 4) +
+          "\n" +
+          "Review successfully added!"
+      );
+  } else {
+    return res.json({ message: "Book does not exist" });
+  }
+});
+
+// Delete review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+
+  if (books[isbn]["reviews"][username]) {
+    delete books[isbn]["reviews"][username];
+    return res
+      .status(200)
+      .send(
+        `${username}'s review deleted!` +
+          "\n" +
+          JSON.stringify(books[isbn], null, 4)
+      );
+  } else {
+    return res
+      .status(200)
+      .json({
+        message: `${username}'s review does not exist for specified book!`,
+      });
+  }
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -5,7 +5,7 @@ let books = {
   4: {
     author: "Unknown",
     title: "The Epic Of Gilgamesh",
-    reviews: { 1: "Praise to the man" },
+    reviews: {},
   },
   5: { author: "Unknown", title: "The Book Of Job", reviews: {} },
   6: { author: "Unknown", title: "One Thousand and One Nights", reviews: {} },
